077c2d9bfb34d3e35562ed2f4d26834364c7335d
hamzatalha77 <hamza_ta7@hotmail.com>
2023-06-30 16:18:50 +0100

    comit

---
+++ src/redux/userSlice.ts
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    name: 'jhon',
+    email: 'hamza@jhon.ma',
+  },
+  reducers: {
+    update: (state, action) => {
+      state.name = action.payload.name
+      state.email = action.payload.email
+    },
+  },
+})
+const { update } = userSlice.actions
+export { userSlice, update }
