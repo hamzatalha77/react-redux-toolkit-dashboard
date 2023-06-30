c19db9b8299537e10d4a8d952b332f62232acc3f
hamzatalha77 <hamza_ta7@hotmail.com>
2023-06-30 16:25:06 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
-import './App.css'
+import Leftbar from './components/leftbar/Leftbar'
+import Navbar from './components/navbar/Navbar'
+import Rightbar from './components/rightbar/Rightbar'
+import Update from './components/update/Update'
+import './app.css'
+
 const App = () => {
-  return <div></div>
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Leftbar />
+        <Update />
+        <Rightbar />
+      </div>
+    </>
+  )
 }
 
 export default App
