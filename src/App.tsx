fbce3d009c7388880a5925d5174d04d02fce92c9
hamzatalha77 <hamza_ta7@hotmail.com>
2023-06-30 19:11:52 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Leftbar from './components/leftbar/Leftbar'
 import Navbar from './components/navbar/Navbar'
 import Rightbar from './components/rightbar/Rightbar'
 import Update from './components/update/Update'
-import './app.css'
+import './App.css'
 
 const App = () => {
   return (
