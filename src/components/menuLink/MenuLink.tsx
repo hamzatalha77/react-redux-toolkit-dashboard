94f0e870f5bdd1d679192a2927034b1045e198e3
hamzatalha77 <hamza_ta7@hotmail.com>
2023-07-01 12:55:39 +0100

    commit

--- src/components/menuLink/MenuLink.tsx
+++ src/components/menuLink/MenuLink.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import './menulink.css'
+import { useSelector } from 'react-redux'
 
-export default function MenuLink({ icon, text }) {
+interface RootState {
+  user: {
+    name: string
+    // other properties of the user state
+  }
+}
+
+export default function MenuLink({ icon, text }: any) {
+  const name = useSelector((state: RootState) => state.user.name)
   return (
     <div className="menulink">
       {icon}
       <span className="menuLinkText">{text}</span>
-      <span className="menuLinkTextName">
-        {text === 'Logout' && '( John )'}
-      </span>
+      <span className="menuLinkTextName">{text === 'Logout' && `${name}`}</span>
     </div>
   )
 }
